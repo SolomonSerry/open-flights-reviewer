f4947bfd7e9933b3d6aaa95234b3fbaebd903113
solomon.serry <solomon.serry@wealthsimple.com>
2023-11-03 14:20:56 -0400

    removed unused code

--- src/App.js
+++ src/App.js
@@ -3,25 +3,7 @@ import Airlines from './componets/Airlines/Airlines';
 import Airline from './componets/Airline/Airline';
 import { Route, Routes } from 'react-router-dom';
 
-// const API_URL = "http://localhost:3000/api/v1/airlines.json"
-
-// function getAPIData() {
-//   return axios.get(API_URL).then((response) => response.data)
-// }
-
 function App() {
-  // const [flights, setFlights] = useState([]);
-
-  // useEffect(()=> {
-  //   let mounted = true;
-  //   getAPIData().then((items) => {
-  //     if (mounted) {
-  //       setFlights(items);
-  //     }
-  //   });
-  //   return () => (mounted = false);
-  // }, []);
-
   return (
     <div className="App">
       <Routes>
